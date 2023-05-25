00620597186be596ca4eae1944f2c16d9a51ad57
Stiv0r <Stiv0r@icloud.com>
2023-05-25 09:12:47 +0300

    praktika3

---
+++ WpfApp1/WpfApp1/Window2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(vod.Text);
+            int y = int.Parse(vod2.Text);
+            int z = int.Parse(vod3.Text);
+            int middle = (x + y + z) - Math.Max(Math.Max(x, y), z) - Math.Min(Math.Min(x, y), z);
+
+            otv.Text = ("Среднее число: " + middle);
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Window5.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window5.xaml
+    /// </summary>
+    public partial class Window5 : Window
+    {
+        public Window5()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOKClick(object sender, RoutedEventArgs e)
+        {
+            int number = Convert.ToInt32(TbNumberA.Text);
+            string description;
+
+            if (number == 0)
+            {
+                description = "нулевое число";
+            }
+            else if (number > 0)
+            {
+                if (number % 2 == 0)
+                {
+                    description = "положительное четное число";
+                }
+                else
+                {
+                    description = "положительное нечетное число";
+                }
+            }
+            else
+            {
+                if (number % 2 == 0)
+                {
+                    description = "отрицательное четное число";
+                }
+                else
+                {
+                    description = "отрицательное нечетное число";
+                }
+            }
+
+            TextBlockAnswer.Text = description;
+        }
+    }
+}
